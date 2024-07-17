0420a39768065faa312ffed77dcb58351efdcec1
RAQKID <officialraqkid@gmail.com>
2024-07-17 14:00:23 +0800

    Update MusicClient.js

--- src/structures/MusicClient.js
+++ src/structures/MusicClient.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { Manager } = require("@lavacord/discord.js");
-const { Client, Collection } = require("discord.js");
-const { promises: { readdir } } = require("fs");
-const { join } = require("path");
-const { LavasfyClient } = require("lavasfy");
+const { Manager } = require('@lavacord/discord.js');
+const { Client, Collection } = require('discord.js');
+const { promises: { readdir } } = require('fs');
+const { join } = require('path');
+const { LavasfyClient } = require('lavasfy');
 
-require("../extensions");
+require('../extensions');
 
 module.exports = class MusicClient extends Client {
     /** @param {import("discord.js").ClientOptions} [opt] */
@@ -14,20 +14,20 @@ module.exports = class MusicClient extends Client {
         this.commands = new Collection();
         this.manager = new Manager(this, [
             {
-                id: "main",
+                id: 'main',
                 host: process.env.LAVA_HOST,
                 port: process.env.LAVA_PORT,
                 password: process.env.LAVA_PASS
             }
         ]);
-        this.spotify = process.env.ENABLE_SPOTIFY === "true"
+        this.spotify = process.env.ENABLE_SPOTIFY === 'true'
             ? new LavasfyClient({
                 clientID: process.env.SPOTIFY_ID,
                 clientSecret: process.env.SPOTIFY_SECRET,
                 playlistLoadLimit: process.env.SPOTIFY_PLAYLIST_PAGE_LIMIT,
                 audioOnlyResults: true,
                 useSpotifyMetadata: true
-            }, [...[...this.manager.nodes.values()]])
+            }, [...this.manager.nodes.values()])
             : null;
 
         this.prefix = process.env.PREFIX.toLowerCase();
@@ -42,15 +42,15 @@ module.exports = class MusicClient extends Client {
         this.login(process.env.TOKEN);
 
         this.manager
-            .on("ready", node => console.log(`Node ${node.id} is ready!`))
-            .on("disconnect", (ws, node) => console.log(`Node ${node.id} disconnected.`))
-            .on("reconnecting", (node) => console.log(`Node ${node.id} tries to reconnect.`))
-            .on("error", (error, node) => console.log(`Node ${node.id} got an error: ${error.message}`));
+            .on('ready', node => console.log(`Node ${node.id} is ready!`))
+            .on('disconnect', (ws, node) => console.log(`Node ${node.id} disconnected.`))
+            .on('reconnecting', node => console.log(`Node ${node.id} tries to reconnect.`))
+            .on('error', (error, node) => console.error(`Node ${node.id} got an error: ${error.message}`));
     }
 
     /** @private */
     async loadCommands() {
-        const commands = await readdir(join(__dirname, "..", "commands"));
+        const commands = await readdir(join(__dirname, '..', 'commands'));
         for (const commandFile of commands) {
             const command = require(`../commands/${commandFile}`);
             this.commands.set(command.name, command);
@@ -59,7 +59,7 @@ module.exports = class MusicClient extends Client {
 
     /** @private */
     async loadEventListeners() {
-        const listeners = await readdir(join(__dirname, "..", "listeners"));
+        const listeners = await readdir(join(__dirname, '..', 'listeners'));
         for (const listenerFile of listeners) {
             const listener = require(`../listeners/${listenerFile}`);
             this.on(listener.name, (...args) => listener.exec(this, ...args));
@@ -69,7 +69,7 @@ module.exports = class MusicClient extends Client {
     /** @private */
     setupExpress() {
         const app = express();
-        const port = 3000;
+        const port = process.env.PORT || 3000;
 
         app.get('/', (req, res) => {
             res.send('Bot is alive!');
